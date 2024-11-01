134e8ef2ed476c85d5c37e06ba3e5420a2e35922
Hritik-rr <hritikranjan17@gmail.com>
2024-11-01 18:05:58 +0530

    model initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { RecruiterModule } from './recruiter/recruiter.module';
+import { CandidateModule } from './candidate/candidate.module';
+import { JobModule } from './job/job.module';
+import { ApplicationModule } from './application/application.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule, RecruiterModule, CandidateModule, JobModule, ApplicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
